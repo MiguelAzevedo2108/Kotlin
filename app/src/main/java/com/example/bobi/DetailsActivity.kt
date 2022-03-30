dabe3f6ca5063c29fa7e6cec56dae9cc86af3750
Miguel Azevedo <miguelazevedo2108@gmail.com>
2022-03-30 15:11:54 +0100

    App with countdown

---
+++ app/src/main/java/com/example/bobi/DetailsActivity.kt
@@ -0,0 +1,14 @@
+package com.example.bobi
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+    }
+
+}
--- app/src/main/java/com/example/bobi/MainActivity.kt
+++ app/src/main/java/com/example/bobi/MainActivity.kt
@@ -1,11 +1,74 @@
 package com.example.bobi
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+
+private const val REQUEST_IMAGE_CAPTURE = 100 //valor qualquer
 
 class MainActivity : AppCompatActivity() {
+
+    private var untilFinished = 10000L
+    private lateinit var timer: CountDownTimer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.button).setOnClickListener{ //ao cliclar no buttao chama a nossa Activity/Page
+            //openPhoneCamera()
+            openDetailsActivity()
+
+        }
+    }
+
+    override fun onResume() {
+        startCountDownTimer(untilFinished)
+        super.onResume()
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        timer.cancel()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) { //este metodo e chamada apos o startActivty result
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            findViewById<ImageView>(R.id.imageView3).setImageBitmap(imageBitmap)
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    private fun openPhoneCamera(){
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE) //usar camera do telemovel
+        startActivityForResult(intent, REQUEST_IMAGE_CAPTURE)
+    }
+
+    private fun openDetailsActivity(){
+        val intent = Intent(this,DetailsActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun startCountDownTimer(time :Long){
+        timer = object: CountDownTimer(time, 10000){
+
+            override fun onTick(millisUntilFinished: Long) {
+                untilFinished = millisUntilFinished
+                findViewById<TextView>(R.id.countdown).text = "Seconds remaining: ${millisUntilFinished/1000}"
+            }
+
+            override fun onFinish() {
+                findViewById<TextView>(R.id.countdown).text = "Done!"
+            }
+        }
+        timer.start()
     }
 }
